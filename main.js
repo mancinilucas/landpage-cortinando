2c2e175001f4d773fcdbde415e19619f40ae1d92
mancinilucas <mancini.lucasg@gmail.com>
2022-02-21 09:58:20 -0300

    swiper bullet

--- main.js
+++ main.js
@@ -69,6 +69,15 @@ mobileNavbar.init()
 /* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
